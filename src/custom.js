3fc7796206127c43ac17e66805c80118b7dd4390
Lucas Maia de Abreu <lucasmaiadeabreu@Air-de-Lucas.lan>
2023-10-19 15:07:41 +0100

    atualização

---
+++ src/custom.js
@@ -0,0 +1,9 @@
+$('document').ready(function(){
+
+    setTimeout(function(){
+         $('#elementoImg1').fadeIn(1000);
+         $('#elementoImg2').fadeIn(3000);
+          $('#elementoImg3').fadeIn(5000);
+    }, 1000);
+ 
+ });
